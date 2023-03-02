cd49686271a0ab122bc784a0ded6ccfa9a511be0
Rhys Smoker <r.smoker@gmail.com>
2023-03-01 22:42:23 -0800

    worked on styling

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-export default function Contact() {
-  return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
-  );
+export default function Porfolio() {
+	return (
+		<div className="d-flex flex-column align-items-center row-cols-2">
+			<h4>React-Bootstrap Form Component</h4>
+			<Form>
+				<Form.Group>
+					<Form.Label>Enter your full name:</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Enter your full name"
+					/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Your email address:</Form.Label>
+					<Form.Control
+						type="email"
+						placeholder="Enter your your email address"
+					/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Enter your message:</Form.Label>
+					<Form.Control
+						type="number"
+						placeholder="your message"
+					/>
+				</Form.Group>
+				<Button
+					variant="primary"
+					type="submit">
+					Click here to submit form
+				</Button>
+			</Form>
+		</div>
+	);
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
 export default function Portfolio() {
 	//array of portfolio object including title, link to repo, etc
@@ -29,18 +30,43 @@ export default function Portfolio() {
 		},
 	];
 
-	//mapping for each component  as in groceries List
+	//single project component used multiple tiems by .map
 	//use card element and class names,
+	//trying out some bootstrapping
 	return (
 		<div>
 			{projects.map((item) => (
-				<div>
-					<h1>{item.title}</h1>
-					<h1>{item.description}</h1>
-					<h1>{item.repo}</h1>
-					<h1>{item.deployed_URL}</h1>
+				//<div class="col-sm-6">
+				//<div class="d-inline-flex p-2">
+				<div class="d-inline-flex p-2">
+					<div class="p-3 mb-2 bg-dark text-white">
+						<Card style={{ width: "20rem" }}>
+							<Card.Body class="p-3 mb-2 bg-dark text-white">
+								<Card.Title>{item.title}</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">
+									Card Subtitle
+								</Card.Subtitle>
+								<Card.Text>{item.description}</Card.Text>
+								<Card.Link href="#">{item.repo}</Card.Link>
+								<Card.Link href="#">{item.deployed_URL}</Card.Link>
+							</Card.Body>
+						</Card>
+					</div>
 				</div>
 			))}
 		</div>
 	);
+	//This is the unstyled version
+	// return (
+	// 	<div>
+	// 		{projects.map((item) => (
+	// 			<div>
+	// 				<h1>{item.title}</h1>
+	// 				<h1>{item.description}</h1>
+	// 				<h1>{item.repo}</h1>
+	// 				<h1>{item.deployed_URL}</h1>
+	// 			</div>
+	// 		))}
+	// 	</div>
+	// );
 }
