1b62da6cd70c14139e8975b6b8c48dea36cd0260
Rhys Smoker <r.smoker@gmail.com>
2023-03-03 13:47:14 -0800

    created icons in footer

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,42 +1,32 @@
 import React from "react";
-import { MDBFooter, MDBIcon } from "mdb-react-ui-kit";
+import { MDBFooter } from "mdb-react-ui-kit";
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaLinkedinIn } from "react-icons/fa";
+import { GoMarkGithub } from "react-icons/go";
+import { FaStackOverflow } from "react-icons/fa";
 
 export default function Footer() {
 	return (
-		<MDBFooter
-			// bgColor="light"
-			className="text-center text-lg-start text-reset">
+		<MDBFooter className="text-center text-lg-start text-reset">
 			<section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
 				<div className="me-5 d-lg-block">
 					<span>Get connected with me on social networks:</span>
 				</div>
-
 				<div>
-					<a
-						href="https://stackoverflow.com/users/9012884/rhys-smoker"
-						className="me-4 text-reset">
-						<MDBIcon
-							icon="camera-retro"
-							//icon="stackoverflow"
-							size="4x"
+					<a href="https://stackoverflow.com/users/9012884/rhys-smoker">
+						<FaStackOverflow />
+					</a>
+
+					<a href="https://www.linkedin.com/in/rhys-smoker/">
+						<a
+							style={{ marginLeft: ".5rem" }}
+							FaLinkedinIn
 						/>
 					</a>
-					{/* <a
-						href="https://www.linkedin.com/in/rhys-smoker/"
-						className="me-4 text-reset">
-						<MDBIcon
-							fab
-							icon="linkedin"
-						/>
+
+					<a href="https://github.com/Noctiluca-scintillans">
+						<GoMarkGithub />
 					</a>
-					<a
-						href="https://github.com/Noctiluca-scintillans"
-						className="me-4 text-reset">
-						<MDBIcon
-							fab
-							icon="github"
-						/>
-					</a> */}
 				</div>
 			</section>
 		</MDBFooter>
